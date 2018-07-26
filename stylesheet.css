086b83cc00bdc0a29f4bfea273f39c906878e3f4
stagmite <41554375+Stagmite@users.noreply.github.com>
2018-07-25 21:08:10 -0400

    Create stylesheet.css

---
+++ stylesheet.css
@@ -0,0 +1,18 @@
+.container {
+  width: 1000px;
+  height: 875px;
+}
+body {
+    background-image: url("https://www.desktopbackground.org/p/2014/01/13/700703_cute-plain-backgrounds-wallpapers-cave_1280x1024_h.jpg");
+    background-repeat: no-repeat;
+    background-size: 4000px 1100px;
+}
+a:link {
+    color: blue;
+}
+a:hover {
+    color: yellow;
+}
+a:active {
+    color: red;
+}
